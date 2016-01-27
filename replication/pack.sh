8228934dfd10cd841ec0329bffbee2e9b020b649
zhangxiaotong3 <zhangxiaotong3@xiaomi.com>
2016-01-27 12:15:25 +0000

    update pack.sh

--- replication/pack.sh
+++ replication/pack.sh
@@ -32,7 +32,7 @@ if [ -z "$BOOST_DIR" ]; then
 fi
 
 version=$1
-BUILD_TYPE=release bash ./build.sh
+# BUILD_TYPE=release bash ./build.sh
 
 # create pack folder
 pack_folder="pegasus-""$version"
@@ -45,14 +45,13 @@ mkdir "$pack_folder"
 mkdir "$pack_folder""/bin"
 cp builder/bin/rrdb/rrdb "$pack_folder""/bin"
 cp client_lib/sample/rrdb_sample "$pack_folder""/bin"
-cp client_lib/sample/config.ini "$pack_folder""/bin"
 cp ../../rDSN/install/bin/dsn.ddlclient/dsn.ddlclient "$pack_folder""/bin"
 cp ../rrdb_bench "$pack_folder""/bin"
 
 # copy tool chain
-mkdir "$pack_folder""/toolchain"
-tool_chain_folder="$(dirname "$TOOLCHAIN_DIR")"
-cp -r "$tool_chain_folder" "$pack_folder""/toolchain"
+# mkdir "$pack_folder""/toolchain"
+# tool_chain_folder="$(dirname "$TOOLCHAIN_DIR")"
+# cp -r "$tool_chain_folder" "$pack_folder""/toolchain"
 
 # copy glibc
 cp -r "$GLIBC_DIR" "$pack_folder""/toolchain"
