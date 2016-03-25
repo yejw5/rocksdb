5071252ae89431ffd68d7284e06d22b650813284
qinzuoyan <qinzuoyan@gmail.com>
2016-03-25 15:41:45 +0800

    improve rrdb_client interfaces

--- replication/client_lib/src/client_factory.cpp
+++ replication/client_lib/src/client_factory.cpp
@@ -8,9 +8,9 @@ bool rrdb_client_factory::initialize(const char* config_file)
     return rrdb_client_factory_impl::initialize(config_file);
 }
 
-irrdb_client* rrdb_client_factory::get_client(const char* app_name, const char* cluster_name)
+irrdb_client* rrdb_client_factory::get_client(const char* app_name, const char* cluster_meta_servers)
 {
-    return rrdb_client_factory_impl::get_client(app_name, cluster_name);
+    return rrdb_client_factory_impl::get_client(app_name, cluster_meta_servers);
 }
 
 }} //namesapce
