252228ba1769eca0450b51ab5178cf67f7fdc258
xiaotz <xiaotz9@gmail.com>
2016-03-30 10:11:42 +0800

    update collector

--- replication/collector/main.cpp
+++ replication/collector/main.cpp
@@ -1,17 +1,14 @@
-#include "client_ddl.h"
 #include <iostream>
+#include "info_collector.h"
+#include <unistd.h>
 
 using namespace dsn::replication;
-
-
+using namespace pegasus::apps;
 
 int init_environment(char* exe, char* config_file)
 {
-    dsn_core_init();
-
     //use config file to run
     char* argv[] = {exe, config_file};
-
     dsn_run(2, argv, false);
     return 0;
 }
@@ -23,8 +20,10 @@ int main(int argc, char** argv)
         std::cerr << "Init failed" << std::endl;
     }
 
+    info_collector collector;
     while(true)
     {
-
+        collector.on_collect();
+        sleep(10);
     }
 }
